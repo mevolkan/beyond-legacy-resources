4792b9c06d3b1a47ac306826faee2ac77b333566
volkan <16579273+mevolkan@users.noreply.github.com>
2023-11-03 17:35:56 +0300

    partials working

--- includes/templates/archive-resource.php
+++ includes/templates/archive-resource.php
@@ -2,7 +2,7 @@
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
     exit;
-}
+    }
 
 get_header();
 $nectar_fp_options = nectar_get_full_page_options();
@@ -10,7 +10,7 @@ $nectar_fp_options = nectar_get_full_page_options();
 <div class="container-wrap">
     <div class="<?php if ($nectar_fp_options['page_full_screen_rows'] !== 'on') {
         echo 'container';
-    } ?> main-content" role="main">
+        } ?> main-content" role="main">
         <div class="<?php echo apply_filters('nectar_main_container_row_class_name', 'row resources'); ?>">
             <?php
             $args = array(
@@ -19,62 +19,12 @@ $nectar_fp_options = nectar_get_full_page_options();
             );
             $loop = new WP_Query($args);
             while ($loop->have_posts()) {
+                if ('' === locate_template('partials/loop.php', true, false))
+                    include('partials/loop.php');
                 $loop->the_post();
-                ?>
-                <div class="resource">
-                    <div class="resource-header">
-
-                        <?php
-                        // Display the post thumbnail
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail('medium');
-                        }
-                        ?>
-                    </div>
-                    <div class="resource-main">
-                        <span class="resource-title">
-                            <?php the_title(); ?>
-                        </span>
-
-                        <?php the_excerpt(); ?>
-                        <?php // Tags
-                            $tags = get_the_tags();
-                            if (!empty($tags)) {
-                                echo '<p>';
-                                foreach ($tags as $tag) {
-                                    // echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a> ';
-                                    echo esc_html($tag->name);
-                                }
-                                echo '</p>';
-                            } ?>
-                        <div class="resource-footer">
-
-                            <?php
-                            // Categories
-                            $categories = get_the_category();
-                            if (!empty($categories)) {
-                                echo '<p>';
-                                foreach ($categories as $category) {
-                                    // echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a> ';
-                                    echo '<span>' . esc_html($category->name) . '</span> ';
-                                }
-                                echo '</p>';
-                            }
-                            // Custom price field
-                            $resource_price = get_post_meta(get_the_ID(), '_resource_price', true);
-                            if (!empty($resource_price)) {
-                                echo '<p class="resource-price">Price: ' . esc_html($resource_price) . '</p>';
-                            } else {
-                                echo '<p class="resource-price">Free</p>';
-                            }
-                            ?>
-                        </div>
-
-                    </div>
-                </div>
-                <?php
-            }
-            wp_reset_postdata(); // Reset the post data to the main loop.
+                get_template_part('partials/loop');
+                }
+            wp_reset_postdata();
             ?>
         </div>
     </div>
---
+++ includes/templates/partials/loop.php
@@ -0,0 +1,51 @@
+<div class="resource">
+    <div class="resource-header">
+
+        <?php
+        // Display the post thumbnail
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('medium');
+        }
+        ?>
+    </div>
+    <div class="resource-main">
+        <span class="resource-title">
+            <?php the_title(); ?>
+        </span>
+
+        <?php the_excerpt(); ?>
+        <?php // Tags
+        $tags = get_the_tags();
+        if (!empty($tags)) {
+            echo '<p>';
+            foreach ($tags as $tag) {
+                // echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a> ';
+                echo esc_html($tag->name);
+            }
+            echo '</p>';
+        } ?>
+        <div class="resource-footer">
+
+            <?php
+            // Categories
+            $categories = get_the_category();
+            if (!empty($categories)) {
+                echo '<p>';
+                foreach ($categories as $category) {
+                    // echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a> ';
+                    echo '<span>' . esc_html($category->name) . '</span> ';
+                }
+                echo '</p>';
+            }
+            // Custom price field
+            $resource_price = get_post_meta(get_the_ID(), '_resource_price', true);
+            if (!empty($resource_price)) {
+                echo '<p class="resource-price">Price: ' . esc_html($resource_price) . '</p>';
+            } else {
+                echo '<p class="resource-price">Free</p>';
+            }
+            ?>
+        </div>
+
+    </div>
+</div>
